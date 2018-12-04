464e27bddf6b719f68e59f77c7fb44f064da0592
Emma Ewert <EmmaEwert@users.noreply.github.com>
2018-12-04 01:21:14 +0100

    Initial Lýsing removal

--- Assets/PURR/Core/Components/Teletype.cs
+++ Assets/PURR/Core/Components/Teletype.cs
@@ -36,8 +36,6 @@
 			{ '~', 5 },
 		};
 
-		public static void Reveal(Lýsing lýsing) => instance.RevealAsync(lýsing);
-
 		public void OnSubmit(BaseEventData eventData) {
 			submit = true;
 		}
@@ -54,48 +52,6 @@
 			}
 			arrow.SetActive(false);
 			submit = false;
-		}
-
-		///<summary>Display characters in the TextMeshPro text one at a time, with sound.</summary>
-		private async void RevealAsync(Lýsing lýsing) {
-			var random = new Unity.Mathematics.Random(BitConverter.ToUInt32(BitConverter.GetBytes(Time.time), 0));
-			using (new SelectedGameObject(gameObject)) {
-				var samplesPerLetter = audioSource.clip.samples / ('Z' - 'A' + 1);
-				var sampleOffset = samplesPerLetter / 32;
-				audioSource.pitch = random.NextFloat(0.5f, 1.5f);
-				text.pageToDisplay = 1;
-				text.maxVisibleCharacters = 0;
-				text.SetText(lýsing.text);
-				//text.SetAllDirty();
-				await NextUpdate();
-				transform.root.gameObject.SetActive(true);
-				do {
-					int firstCharacterIndexOfNextPage;
-					if (text.pageToDisplay > text.textInfo.pageCount - 1) {
-						firstCharacterIndexOfNextPage = text.textInfo.characterCount - 1;
-					} else {
-						firstCharacterIndexOfNextPage = text.textInfo.pageInfo[text.pageToDisplay].firstCharacterIndex;
-					}
-					if (firstCharacterIndexOfNextPage > 0 && firstCharacterIndexOfNextPage < text.maxVisibleCharacters) {
-						await WaitForSubmit();
-						text.pageToDisplay++;
-					} else if (lýsing.pauses.Contains(text.maxVisibleCharacters)) {
-						await WaitForSubmit();
-					}
-					++text.maxVisibleCharacters;
-					var c = lýsing.plainText.ToUpper()[text.maxVisibleCharacters - 1];
-					if (c >= 'A' && c <= 'Z') {
-						audioSource.timeSamples = mad((c - 'A'), samplesPerLetter, sampleOffset);
-						audioSource.Play();
-					}
-					await NextUpdate();
-					if (timings.TryGetValue(c, out var timing)) {
-						await new WaitForFrames(timing);
-					}
-				} while (text.maxVisibleCharacters < text.textInfo.characterCount);
-				await WaitForSubmit();
-			}
-			transform.root.gameObject.SetActive(false);
 		}
 	}
 }
